1b18f61adcc6b6a641fa34ec999dde4247043cde
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-03 10:34:43 +0200

    fixbug

--- RandomShips/StartUp.cs
+++ RandomShips/StartUp.cs
@@ -8,34 +8,27 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Select min tier ship: ");
-            string Tier;
-            int minTier;
-            bool parseShip;
-            InputMinTier(out Tier, out minTier, out parseShip);
+
+            InputMinTier( out string Tier, out int minTier, out bool parseShip);
 
             Console.WriteLine("Select max tier ship: ");
 
             int maxTier = InputMaxTier(ref Tier, minTier, ref parseShip);
-
 
             var random = new Random();
             var randomShips = random.Next(minTier, maxTier);
 
             GetWriteNations();
 
-
             var nations = Console.ReadLine();
-
             nations = GetNations(nations);
 
             GetWriteType();
-
 
             var ship = Console.ReadLine();
             ship = GetTypeShip(ship, nations);
 
             Console.WriteLine($"Play with {randomShips} tier {nations} {ship}.");
-
         }
 
 
