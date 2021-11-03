f8dc29dfbd157b12a708f15e4b65fcd0dc558456
Svetoslav Gochev <60218470+SvetoslavGochev@users.noreply.github.com>
2021-11-03 10:03:16 +0200

    Add new classs
    gg

---
+++ RandomShips/ShipsExtensionss.cs
@@ -0,0 +1,12 @@
+﻿namespace RandomShips
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Ships
+    {
+    }
+}
---
+++ RandomShips/StartUp.cs
@@ -0,0 +1,45 @@
+﻿namespace RandomShips
+{
+    using System;
+    using static ShipsExtensionss;
+
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select min tier ship: ");
+            string Tier;
+            int minTier;
+            bool parseShip;
+            InputMinTier(out Tier, out minTier, out parseShip);
+
+            Console.WriteLine("Select max tier ship: ");
+
+            int maxTier = InputMaxTier(ref Tier, minTier, ref parseShip);
+
+
+            var random = new Random();
+            var randomShips = random.Next(minTier, maxTier);
+
+            GetWriteNations();
+
+            var nations = "";
+            nations = Console.ReadLine();
+
+            nations = GetNations(nations);
+
+            GetWriteType();
+
+            var ship = "";
+            ship = Console.ReadLine();
+            ship = GetTypeShip(ship, nations);
+
+            Console.WriteLine($"Play with {randomShips} tier {nations} {ship}.");
+
+        }
+
+   
+    }
+}
+
+  
